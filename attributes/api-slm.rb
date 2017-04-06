d2275f3b59655a9e62382eccc08c3c33a71177c4
Dylan Myers <dylan.myers@bluemedora.com>
2017-04-06 14:24:58 -0400

    Working API-S/SLM Install

--- attributes/api-slm.rb
+++ attributes/api-slm.rb
@@ -6,13 +6,20 @@
 ##
 
 # Maintenance User
-default[:netapp][:api][:username] = "maint"
-default[:netapp][:api][:password] = "some_password"
+# default[:netapp][:api][:username] = "maint"
+default[:netapp][:api][:password] = "SomePassword1!"
   
 # Port
 default[:netapp][:api][:port] = "8443"
-  
-# Location of the installer media
-default[:netapp][:api][:media] = ''
 
-default[:netapp][:api]
+# Media filename
+default[:netapp][:api][:media_filename] = 'FileName.bin'
+
+# Media path
+default[:netapp][:api][:media_path] = '/some/mount/point/'
+
+# Location of the installer media
+default[:netapp][:api][:media] = "#{node[:netapp][:api][:media_path]}#{node[:netapp][:api][:media_filename]}"
+
+# Temporary location
+default[:netapp][:api][:tmp] = '/tmp/netapp-media'
--- recipes/api-slm.rb
+++ recipes/api-slm.rb
@@ -4,3 +4,36 @@
 #   Dylan Myers
 # Copyright 2017 Blue Medora
 ##
+
+# Create a /tmp/netapp-media folder to work from
+directory node[:netapp][:api][:tmp] do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+remote_file "#{node[:netapp][:api][:tmp]}/#{node[:netapp][:api][:media_filename]}" do
+  source "file:///#{node[:netapp][:api][:media]}"
+  owner 'root'
+  group 'root'
+  mode '0500'
+  action :create_if_missing
+end
+
+execute 'Install NetApp API-S/SLM' do
+  command "./#{node[:netapp][:api][:media_filename]} -p #{node[:netapp][:api][:password]} -t #{node[:netapp][:api][:port]} -y"
+  cwd node[:netapp][:api][:tmp]
+end
+
+file "#{node[:netapp][:api][:tmp]}/#{node[:netapp][:api][:media_filename]}" do
+  action :delete
+end
+
+# Delete the /tmp/netapp-media folder
+directory node[:netapp][:api][:tmp] do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :delete
+end
